759adc7ab513ab75c96fa9d997820ef3cfd97535
DESKTOP-0BK3HD8\HiImMike <mikeraadsheer11@gmail.com>
2021-04-07 11:30:16 +0200

    Zeemijn detectie met raycast kompleet.
    Drone movement verder maken in nieuwe branch. vervolgens quick time event maken.

---
+++ Assets/ZeemijnDetectie.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeemijnDetectie : MonoBehaviour
+{
+    private InputManager _input;
+    private float _range = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, _range))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
+            Debug.Log(hit.transform.name);
+        }
+
+    }
+}
