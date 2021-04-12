b477c171b7910334f5093cf1b9deda0c94459937
DESKTOP-0BK3HD8\HiImMike <mikeraadsheer11@gmail.com>
2021-04-12 16:48:27 +0200

    Deel van zeemijn interactie code gemaakt.

--- Assets/ZeemijnDetectie.cs
+++ Assets/ZeemijnDetectie.cs
@@ -1,28 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ZeemijnDetectie : MonoBehaviour
 {
     private InputManager _input;
-    private float _range = 10f;
+    private float _range = 5f;
+    public Action PlantExplosive;
+    private bool detectedMine = false;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        _input.Interact += StartQuickTimeEvent;
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         RaycastHit hit;
 
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, _range))
         {
+            // Draw line in scene view to visualize raycast
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
-            Debug.Log(hit.transform.name);
+            detectedMine = true;
         }
+        else
+        {
+            detectedMine = false;
+        }
+    }
 
+    void StartQuickTimeEvent()
+    {
+        Debug.Log("Test");
+        if (detectedMine)
+        {
+            Debug.Log("Good Test");
+        }
     }
 }
