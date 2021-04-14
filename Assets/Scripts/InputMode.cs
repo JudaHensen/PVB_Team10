218ed5ffedf3a7182e184099e77344bb21668f3c
DESKTOP-0BK3HD8\HiImMike <mikeraadsheer11@gmail.com>
2021-04-14 14:26:02 +0200

    Zeemijn spawning code klaar en 100% dynamiche. scructuur is te zijn in de scene ZeemijnSpawning

--- Assets/Scripts/InputMode.cs
+++ Assets/Scripts/InputMode.cs
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-﻿public enum InputMode { GAMEPLAY, QUICK_TIME }
-=======
-﻿public enum InputMode { GAMEPLAY, QUICK_TIME }
->>>>>>> development
+public enum InputMode { GAMEPLAY, QUICK_TIME }
