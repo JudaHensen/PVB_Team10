086ae0f5bf7dd04a2e4710fb06fd8c5ff2184757
Sjors van der Kleij <Sjorsvanderkleij2001@gmail.com>
2021-04-16 14:48:19 +0200

    applied surface water shader worked on cameramovement when accelerating

--- Assets/Scripts/CameraSpeed.cs
+++ Assets/Scripts/CameraSpeed.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class CameraSpeed : MonoBehaviour
 {
     private InputManager _input;
-    Camera camera;
+    public Transform start;
+    public Transform end;
+    private Camera camera;
+    private float speed = 1;
     void Start()
     {
         _input = FindObjectOfType<InputManager>();
         camera = FindObjectOfType<Camera>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_input.TriggerRight >= 0.1)
+        {
+            transform.position = Vector3.Lerp(start.position, end.position, speed * Time.deltaTime);
+            Debug.Log("eeee");
+        }
+        if(_input.TriggerRight == 0)
+        {
+            transform.position = Vector3.Lerp(end.position, start.position, speed * Time.deltaTime);
+        }
         
     }
 }
