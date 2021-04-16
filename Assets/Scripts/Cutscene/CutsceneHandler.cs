0b132875cba796b6a94a485f58f193ae7b8d421a
JudaHensen <24311@ma-web.nl>
2021-04-16 16:53:59 +0200

    Completed most of the cutscene system. - Code needs to be cleaned up. - Needs to merge with Volkens code. - Need to test animation triggers.

---
+++ Assets/Scripts/Cutscene/CutsceneHandler.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading.Tasks;
+
+namespace Cutscene
+{
+    public class CutsceneHandler : MonoBehaviour
+    {
+        [Header("All transitions in chronological order.")]
+        [SerializeField]
+        private List<GameObject> _transitions;
+
+        public Action OnFinished;
+
+        private Camera _startCamera;
+        private Camera _cameraReplica;
+        private Camera _currentCamera;
+
+        private bool _isPlaying = false;
+
+        private void Start()
+        {
+            _startCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+            _startCamera.enabled = true;
+
+            for (int i = 0; i < Camera.allCamerasCount; ++i)
+            {
+                if(Camera.allCameras[i] != _startCamera) Camera.allCameras[i].enabled = false;
+            }
+
+            GameObject.Find("InputHandler").GetComponent<InputManager>().Interact += StartCutscene;
+        }
+
+
+        public void StartCutscene()
+        {
+            if(!_isPlaying)
+            {
+                Debug.Log("Starting cutscene");
+                _isPlaying = true;
+
+                _startCamera.enabled = false;
+
+                transform.position = _startCamera.transform.position;
+
+                GameObject camParent = new GameObject("CameraReplica");
+                camParent.transform.parent = transform;
+
+                _cameraReplica = camParent.AddComponent<Camera>();
+                _cameraReplica.CopyFrom(_startCamera);
+                _cameraReplica.enabled = true;
+
+                _currentCamera = _cameraReplica;
+
+                ExecuteCutscene();
+            }
+        }
+
+        private async void ExecuteCutscene()
+        {
+            for(int i = 0; i < _transitions.Count; ++i)
+            {
+                Transition transition = _transitions[i].GetComponent<Transition>();
+
+                transition.Execute(this);
+                await Task.Delay(transition.GetDelay());
+
+                //Debug.Log($"Transition {transition.GetName()} finished.");
+            }
+            Debug.Log("Cutscene finished!");
+
+            _currentCamera.enabled = false;
+            _startCamera.enabled = true;
+
+            _cameraReplica = null;
+            _currentCamera = null;
+
+            Destroy(transform.Find("CameraReplica").gameObject);
+
+            for (int i = 0; i < _transitions.Count; ++i)
+            {
+                _transitions[i].GetComponent<Transition>().Reset();
+            }
+
+            _isPlaying = false;
+
+            try {
+                OnFinished();
+            } catch(Exception e) {
+                // No functions assigned to action
+                Debug.Log(e.Message);
+            }
+        }
+
+        public Camera GetCamera() { return _cameraReplica; }
+        public Camera GetCurrentCamera() { return _currentCamera; }
+        public void SetCurrentCamera(Camera cam) { _currentCamera = cam; }
+
+    }
+}
+
---
+++ Assets/Scripts/Cutscene/Transition.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cutscene
+{
+    public class Transition : MonoBehaviour
+    {
+        [Header("Transition name for debugging.")]
+        [SerializeField] private string _name;
+
+        [Header("Transition time in miliseconds.")]
+        [SerializeField] private int _length;
+
+#region Move & Rotation
+
+        [Header("If from is empty, move & rotate from curent camera transform.")]
+        [Space(-10)]
+        [Header("Move and rotate camera from & to a GameObjects transform.")]
+        [SerializeField] private GameObject _moveFrom;
+
+        [SerializeField] private GameObject _moveTo;
+
+        [Header("Rotate to toTransform's rotation.")]
+        [SerializeField] private bool _enableRotation = true;
+
+        [Header("True if the object is a child of a moving object, false if the object is static.")]
+        [SerializeField] private bool _local;
+
+        [Header("Time in seconds to move and rotate.")]
+        [SerializeField] public float _timespan;
+
+#endregion
+
+        [Header("If filled, make this the active camera.")]
+        public Camera _camera;
+
+        [Header("List of animations to play for this current transition.")]
+        [SerializeField] private List<Cutscene.Animation> _animations;
+
+        private AdjustCamera _cameraAdjuster;
+
+        private Vector3 _positionSave;
+        private Quaternion _rotationSave;
+
+        private void Awake()
+        {
+            _cameraAdjuster = GameObject.Find("CutsceneManager").GetComponent<AdjustCamera>();
+            if(_camera && _moveFrom)
+            {
+                _camera.transform.localPosition = _moveFrom.transform.localPosition;
+                _camera.transform.localRotation = _moveFrom.transform.localRotation;
+            }
+        }
+
+        public int GetDelay() { return _length + 50; }
+        public string GetName() { return _name; }
+
+        public void Execute(CutsceneHandler cutsceneHandler)
+        {
+            Debug.Log($"> Executing transition: {_name}");
+
+            // Jump to new camera if needed
+            if (!_moveFrom) _moveFrom = cutsceneHandler.GetCurrentCamera().gameObject;
+
+            if (_camera)
+            {
+                cutsceneHandler.GetCurrentCamera().enabled = false;
+                _camera.enabled = true;
+                
+                cutsceneHandler.SetCurrentCamera(_camera);
+                _moveFrom = _camera.gameObject;
+            }
+            _cameraAdjuster.SetCamera(cutsceneHandler.GetCurrentCamera());
+
+            // Save position and rotation for replay;
+            _positionSave = _moveFrom.transform.localPosition;
+            _rotationSave = _moveFrom.transform.localRotation;
+
+            // Move & rotate camera if needed
+            if (_moveTo)
+            {
+                _cameraAdjuster.Adjust(_moveFrom.transform, _moveTo.transform, _enableRotation, _local, _timespan);
+            }
+
+            // Trigger animations
+            if (_animations.Count > 0)
+            {
+                for (int i = 0; i < _animations.Count; ++i)
+                {
+                    Cutscene.Animation anim = _animations[i];
+                    anim.animator.SetTrigger(anim.triggerName);
+                }
+            }
+
+        }
+
+        public void Reset()
+        {
+            _moveFrom.transform.localPosition = _positionSave;
+            _moveFrom.transform.localRotation = _rotationSave;
+        }
+
+    }
+}
+
